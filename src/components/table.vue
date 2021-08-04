dfdb221acdf34724aee0697b420621ec0ae4c790
yj <1833>
2021-08-04 14:44:13 +0800

    table序号

--- src/components/table.vue
+++ src/components/table.vue
@@ -2,6 +2,11 @@
     <div>
         <slot name="header"></slot>
         <el-table :data="tableData" border style="width: 100%">
+            <el-table-column prop="" label="序号" width="180">
+                <template slot-scope="scope">
+                    <span>{{scope.$index + 1}}</span>
+                </template>
+            </el-table-column>
             <el-table-column prop="date" label="日期" width="180">
             </el-table-column>
             <el-table-column prop="name" label="姓名" width="180">
