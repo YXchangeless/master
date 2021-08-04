bee08de056ee994f2caea4b06682408146b5777e
yj <1833>
2021-08-04 18:02:39 +0800

    xiugai

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,110 +1,104 @@
 <template>
-  <div>
-    <!-- <Add></Add> -->
-    <!-- <Sub></Sub> -->
-    <Table :tableData="tableData" @ss="getss">
-      <template slot="header">
-        <el-row style="padding: 10px 0;">
-          <el-col :span="6">
-            <el-select v-model="value" placeholder="请选择">
-              <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-              </el-option>
-            </el-select>
-          </el-col>
-          <el-col :span="6">
-            <el-select v-model="value" placeholder="请选择">
-              <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-              </el-option>
-            </el-select>
-          </el-col>
-          <el-col :span="6">
-            <el-select v-model="value" placeholder="请选择">
-              <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
-              </el-option>
-            </el-select>
-          </el-col>
-          <el-col :span="6">
-            <el-button type="primary" @click="ss">查询</el-button>
-            <el-button type="info" @click="reset">重置</el-button>
-          </el-col>
-        </el-row>
-      </template>
-    </Table>
-  </div>
+  <el-container class="container">
+    <!-- 头部 -->
+    <el-header>
+      <div>
+        <img
+          src="https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg"
+          class="img"
+        />
+        <span>后台管理系统</span>
+      </div>
+      <el-button type="info">退出</el-button>
+    </el-header>
+    <!-- 主体 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+        >
+          <el-submenu index="2">
+            <!-- 一级菜单 -->
+            <template slot="title">
+              <i class="el-icon-user-solid"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="3">用户列表</el-menu-item>
+          </el-submenu>
+           <el-submenu index="4">
+            <!-- 一级菜单 -->
+            <template slot="title">
+              <i class="el-icon-user-solid"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="5">用户列表</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="6">
+            <i class="el-icon-setting"></i>
+            <span slot="title">设置</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <!-- 内容 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-  import Input from '../components/input.vue'
-  // import Add from '../components/add.vue'
-  // import Add from '@/components/add.vue'
-  import Sub from '../components/sub.vue'
-  import Table from '../components/table.vue'
-  export default {
-    name: "demo",
-    components: {
-      Input,
-      // Add,
-      Sub,
-      Table
-    },
-    data() {
-      return {
-        name: 'sssss',
-        button: [1, 2, 3],
-        tableData: [],
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: ''
-      };
-    },
-    created() {},
-    mounted() {
-      this.ss();
-    },
-    methods: {
-      getss(a,b) {
-        console.log(a,b);
-      },
-      getVal(a,b) {
-        console.log(a,b);
-      },
-      ss() {
-        this.tableData = [
-          {
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }
-        ]
-      },
-    },
-  };
+import Input from '../components/input.vue'
+// import Add from '../components/add.vue'
+// import Add from '@/components/add.vue'
+import Sub from '../components/sub.vue'
+import Table from '../components/table.vue'
+export default {
+  name: 'demo',
+  components: {
+    Input,
+    // Add,
+    Sub,
+    Table,
+  },
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+}
 </script>
-<style scoped>
+<style lang="scss" scoped>
+.container {
+  height: 100%;
+}
+.img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
