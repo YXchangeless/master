ea741db2b2b57f352ee71308e5edd2517698d982
yj <1833>
2021-08-11 09:46:53 +0800

    修改

--- src/view/User.vue
+++ src/view/User.vue
@@ -1,10 +1,25 @@
 <template>
     <div>
-        用户列表
+        <el-input v-model="input" :placeholder="placeholder"></el-input>
+
     </div>
 </template>
 <script>
 export default {
+    data() {
+    return {
+        input:'',
+        placeholder: '请输入内容',
+    }
+  },
+  created() {
+      if(this.input == '') {
+          this.placeholder = ''
+      }
+  },
+  methods: {
+
+  }
 
 }
 </script>
