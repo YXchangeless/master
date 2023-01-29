3ee82ae3d3f6b3bc03c44a28b0a7f2f1566855aa
yaojie <jie.yao@52ydwf.com>
2023-01-29 10:25:21 +0800

    11

--- src/view/User.vue
+++ src/view/User.vue
@@ -3,13 +3,22 @@
     <el-row>
       <el-col :span="24">computed计算属性的使用</el-col>
       <el-col :span="24">Original message: "{{ message }}"</el-col>
-      <el-col :span="24">Computed reversed message: "{{ reversedMessage }}"</el-col>
+      <el-col :span="24">
+        Computed reversed message: "{{ reversedMessage }}"
+      </el-col>
     </el-row>
-     <el-divider></el-divider>
+    <el-divider></el-divider>
     <el-row>
-        <el-col :span="24">mixins混入的使用</el-col>
-        <el-col :span="24"><el-button @click="getButton">mixins</el-button></el-col>
-        <el-col :span="24">{{msg}}</el-col>
+      <el-col :span="24">mixins混入的使用</el-col>
+      <el-col :span="24">
+        <el-button @click="getButton">mixins</el-button>
+      </el-col>
+      <el-col :span="24">{{ msg }}</el-col>
+    </el-row>
+    <el-divider></el-divider>
+    <el-row>
+      <el-col :span="24">filters混入的使用</el-col>
+      <el-col :span="24">{{ f | getFilters }}</el-col>
     </el-row>
   </div>
 </template>
@@ -21,6 +30,7 @@ export default {
   data() {
     return {
       message: 'Hello',
+      f: 'filters',
     }
   },
   computed: {
@@ -28,11 +38,16 @@ export default {
       return this.message + '元'
     },
   },
+  filters: {
+    getFilters(val) {
+      return val + '的使用'
+    },
+  },
   created() {},
   methods: {
-      getButton(){
-          this.mixinMethod()
-      }
+    getButton() {
+      this.mixinMethod()
+    },
   },
 }
 </script>
